b0048fd40744f4ed9176f1e3809e70702cb1cadd
liuyang <981872372@qq.com>
2019-02-25 21:38:09 +0800

    Initial commit

---
+++ app/src/main/java/com/ehenjoom/andrid/picpick/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ehenjoom.andrid.picpick;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
